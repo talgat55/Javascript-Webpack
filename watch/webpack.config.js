4da996396100fd3af2bebddaebbf06d7b04be978
talgat55 <lightx_design@yahoo.com>
2016-11-08 15:58:04 +0600

    add watch files and change time before reload files

---
+++ watch/webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+ 
+module.exports = { 
+
+  entry:  "./home", 
+
+  output: { 
+
+    filename: "build.js",
+    library: "home"
+
+  },
+  watch: true,
+  watchOptions:{
+  	aggregateTimeout: 100
+  } 
+  
+}
