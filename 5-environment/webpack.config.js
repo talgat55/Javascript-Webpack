1903bafe6b5327cca73a873f6383f1cc6c44fb1c
talgat55 <lightx_design@yahoo.com>
2016-11-08 20:09:25 +0600

    add environment in server and client side

---
+++ 5-environment/webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+  
+  const NODE_END = process.env.NODE_ENV || "development";
+  const webpack = require("webpack");
+module.exports = { 
+
+  entry:  "./home", 
+
+  output: { 
+
+    filename: "build.js",
+    library: "home"
+
+  },
+  watch: NODE_ENV == "development",
+
+  watchOptions:{
+  	aggregateTimeout: 100
+  },
+  devtool: NODE_ENV == "development" ? "source-map" : null,
+  plugins:[
+    new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(NODE_ENV),
+        LANG: JSON.stringify("ru")
+    }) 
+  ]
+  
+}
---
+++ 5-environment/welcome.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export default function(message) {
+
+  if(NODE_ENV=="development"){
+
+  	console.log(message);
+
+  }
+
+  alert(`Welcome ${message}`);
+};
